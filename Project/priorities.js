fb23e97928ea708c6efd30277ffdbcc3f5c793dc
Jalenbuff <jalenbuffert@gmail.com>
2026-07-09 12:59:10 -0500

    Added Priorities and Updated README

--- Project/priorities.js
+++ Project/priorities.js
@@ -1,1 +1,12 @@
-// Intitial Doc
+const career = ['Research Companies', ' Submit Applications', 'Follow up with Recruiters', ' Reach out to Jermee for lunch resched'];
+const Learning = ['Find new book', 'Finish node learning lesson','Read Chapter', 'Update Efficient Frontier Project'];
+const Projects = ['JalenOS', 'Portfolio', 'AWS'];
+const Investments = ['Review Roth', 'Review Earnings', 'Macro News', 'Review Individual Acc'];
+
+function display(priorities) {
+    // for (let i = 0; i<priorities.length; i++) {    
+    // }
+    console.log(priorities.forEach(priorities =>console.log(color)));
+}
+
+console.log('Hello, {name}');
