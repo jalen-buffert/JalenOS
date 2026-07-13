e5bee416fd2522b4a0de8efd50406b7f96a54890
Jalenbuff <jalenbuffert@gmail.com>
2026-07-13 12:43:15 -0500

    Split priorities and displaying priorities into different modules

---
+++ Project/display_priorities.js
@@ -0,0 +1,21 @@
+const Career = ['Research Companies', ' Submit Applications', 'Follow up with Recruiters', ' Reach out to Jermee for lunch resched'];
+const Learning = ['Find new book', 'Finish node learning lesson','Read Chapter', 'Update Efficient Frontier Project'];
+const Projects = ['JalenOS', 'Portfolio', 'AWS'];
+const Investments = ['Review Roth', 'Review Earnings', 'Macro News', 'Review Individual Acc'];
+
+
+
+function display(priorities) {
+    for(i = 0 ; i < priorities.length; i ++){
+        console.log(priorities.forEach(priorities =>console.log(priorities)));
+    }
+    return priorities
+    
+}
+
+console.log(display([Career,Learning,Projects,Investments]));
+
+// Objects 
+// Modules
+// File system
+// HTTP Server 
---
+++ Project/priority_list.js
@@ -0,0 +1,100 @@
+// Career Priorities 
+const pc1 = { 'Title':'Company Research',
+                    'Level of Importance':'High',
+                    'Category':'Career',
+                    'Action':'Apply to XXXX, and research XXX until you find 10 jobs then reach out to 3 people within company.',
+                    'Latest Status': 'Connected with XXXX and XXX, but also have not applied to any of the 4 positions.',
+                    'Quick Link': '.....'
+}
+
+const pc2 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+
+//Learning 
+const pl1 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+
+const pl2 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+
+const pl3 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+
+const pl4 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+// Project Priorities 
+const pp1 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+const pp2 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+const pp3 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+// Investment Priorities 
+const pi1 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+const pi2 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+const pi3 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
+const pi4 = { 'Title':'Stay Connected!',
+            'Level of Importance':'Medium',
+            'Category':'Career',
+            'Action':'Reach out to since it has been XXXX Days since last contact',
+            'Latest Status':'Recently Spoke to XXXX Need to speak to XXXX',
+            'Quick Link':'.....'
+}
